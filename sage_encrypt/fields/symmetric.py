b18e6a83fcb256ee03a0469ee58fd333af6d35c4
mehran rahmanzadeh <mehran@theivan.org>
2021-06-19 16:15:54 +0430

    feat (sage_encrypt--fields): Add symmetric fields
    - create symmetric encrypted fields from django fields

---
+++ sage_encrypt/fields/symmetric.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from sage_encrypt.mixins.cast import INT, BIGINT, DATE, TIMESTAMP, BOOL, DOUBLE, TIME
+from sage_encrypt.mixins.encrypt import EncryptSymmetricMixin
+from sage_encrypt.mixins.query import SYM_ENCRYPT_SQL_WITH_NULLIF
+
+
+class EncryptedEmailField(EncryptSymmetricMixin, models.EmailField):
+    pass
+
+
+class EncryptedIntegerField(EncryptSymmetricMixin, models.IntegerField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = INT
+
+
+class EncryptedBigIntegerField(EncryptSymmetricMixin, models.BigIntegerField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = BIGINT
+
+
+class EncryptedTextField(EncryptSymmetricMixin, models.TextField):
+    pass
+
+
+class EncryptedCharField(EncryptSymmetricMixin, models.CharField):
+    pass
+
+
+class EncryptedDateField(EncryptSymmetricMixin, models.DateField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = DATE
+
+
+class EncryptedDateTimeField(EncryptSymmetricMixin, models.DateTimeField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = TIMESTAMP
+
+
+class EncryptedBooleanField(EncryptSymmetricMixin, models.BooleanField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = BOOL
+
+
+class EncryptedFloatField(EncryptSymmetricMixin, models.FloatField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = DOUBLE
+
+
+class EncryptedTimeField(EncryptSymmetricMixin, models.TimeField):
+    encrypt_query = SYM_ENCRYPT_SQL_WITH_NULLIF
+    cast = TIME
