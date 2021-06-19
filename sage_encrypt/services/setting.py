f760ab64bab1025870263734cc5e9e005bcd9538
mehran rahmanzadeh <mehran@theivan.org>
2021-06-19 18:17:39 +0430

    fix (sage_encrypt--services): Fix get_setting
    - get setting from app setting

--- sage_encrypt/services/setting.py
+++ sage_encrypt/services/setting.py
@@ -1,8 +1,11 @@
-from django.conf import settings
+from sage_encrypt import settings
 
 def get_setting(connection, key):
     """
     get key from connection or default to settings.
+    :type connection
+    :type key: str
+    :rtype setting value
     """
     if key in connection.settings_dict:
         return connection.settings_dict[key]
