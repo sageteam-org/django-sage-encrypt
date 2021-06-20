05358a30faf837a8828fe4f93893dc526e80331b
mehran rahmanzadeh <mehran@theivan.org>
2021-06-20 20:37:14 +0430

    fix (sage_encrypt--setting): Fix setting
    -  add public and private key

--- sage_encrypt/settings.py
+++ sage_encrypt/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
 ENCRYPT_KEY = settings.ENCRYPT_KEY if hasattr(settings, 'ENCRYPT_KEY') else settings.SECRET_KEY
-ENCRYPT_PRIVATE_KEY = settings.ENCRYPT_KEY if hasattr(settings, 'ENCRYPT_PRIVATE_KEY') else settings.SECRET_KEY
-ENCRYPT_PUBLIC_KEY = settings.ENCRYPT_KEY if hasattr(settings, 'ENCRYPT_PUBLIC_KEY') else settings.SECRET_KEY
+ENCRYPT_PRIVATE_KEY = settings.ENCRYPT_PRIVATE_KEY if hasattr(settings, 'ENCRYPT_PRIVATE_KEY') else None
+ENCRYPT_PUBLIC_KEY = settings.ENCRYPT_PUBLIC_KEY if hasattr(settings, 'ENCRYPT_PUBLIC_KEY') else None
